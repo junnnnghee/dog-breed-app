83f64aca191c6ac952b5762bd69e1725f56d970d
junnnnghee <qhrehlwl111@gmail.com>
2025-02-05 04:29:36 +0900

    d

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import pandas as pd
 def main():
     st.title('강아지 정보 앱')
     st.subheader('강아지에 대한 정보를 제공하는 앱입니다.')
-    st.text('메뉴를 선택해주세요.')
     
-    df = pd.read_csv('dog/Car_Purchasing_Data.csv')
-    print(df)
-    st.dataframe(df)
+    menu = ['홈', '품종 정보']
+    st.sidebar(menu)
+
 
 
 
